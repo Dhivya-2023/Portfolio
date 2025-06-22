10f55b178362ab1802b80be0986daeb32aa2cd9a
Dhivya-2023 <dhivyanagaraj2720@gmail.com>
2025-06-23 00:12:42 +0530

    Publish

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-     base: command === 'serve' ? '/' : '/Portfolio/',
+  base: command === 'serve' ? '/' : '/Portfolio/',
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+    alias: { '@': path.resolve(__dirname, './src') },
+  },
+  build: {
+    sourcemap: false,    // ← turn off source-maps in prod
   },
 }))

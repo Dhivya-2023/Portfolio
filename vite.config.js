63a510ea2f6d5c617fad37169a422260a9cfe637
Dhivya-2023 <dhivyanagaraj2720@gmail.com>
2025-06-23 12:11:55 +0530

    Add base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 export default defineConfig(({ command }) => ({
 
   plugins: [react()],
-
+  base: '/Portfolio/', // Add this line - this is the key fix
   build: {
     sourcemap: false,    // ← turn off source-maps in prod
      outDir: 'dist',  

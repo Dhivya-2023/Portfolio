2595efcba45f8b273091483de7e7773a6da15f6b
Dhivya-2023 <dhivyanagaraj2720@gmail.com>
2025-10-09 23:23:56 +0530

    update confug

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Portfolio/", // 👈 add this line (your repo name)
   server: {
     host: "::",
     port: 8080,

5f7f91f3f6392cfef9a43a7bfef584887ee133a1
Dhivya-01 <dhivya.n@rbg.ai>
2025-10-10 17:47:12 +0530

    add url in config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
- 
+  base: "/Portfolio/", // 👈 REQUIRED for GitHub Pages (matches your repo name)
   server: {
     host: "::",
     port: 8080,
